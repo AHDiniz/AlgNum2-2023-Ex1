6dcb2fc99a045e581808d9132b279e56361bb97a
Alan Herculano Diniz <alanhdiniz@gmail.com>
2023-04-08 18:34:50 -0300

    Adding matrices with better graphics

--- ex1.m
+++ ex1.m
@@ -1,21 +1,29 @@
-files = {"bcsstm02", "gre_115", "1138_bus"};
-tol = cell(3, 1);
-max_iter = cell(3, 1);
-omega = cell(3, 1);
+files = {"bcsstm02", "gre_115", "1138_bus", "steam3", "494_bus"};
+tol = cell(5, 1);
+max_iter = cell(5, 1);
+omega = cell(5, 1);
 
-tol{1} = 0.0001;
-max_iter{1} = 1000;
-omega{1} = 0.8;
+tol{1} = 0.000001;
+max_iter{1} = 100;
+omega{1} = 1.2;
 
 tol{2} = 0.0001;
-max_iter{2} = 1000;
+max_iter{2} = 70;
 omega{2} = 0.8;
 
 tol{3} = 0.0001;
-max_iter{3} = 1000;
-omega{3} = 0.8;
+max_iter{3} = 360;
+omega{3} = 0.4;
 
-for i = 1 : 3
+tol{4} = 0.000001;
+max_iter{4} = 90;
+omega{4} = 0.8;
+
+tol{5} = 0.00001;
+max_iter{5} = 2000;
+omega{5} = 0.8;
+
+for i = 5
 
     filename = sprintf("in/%s.mat", files{i});
 
